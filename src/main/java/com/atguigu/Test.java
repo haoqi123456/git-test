b11c40d7d8af0684ee3bf5e950640f3aedfacd45
ljx <1845617678@qq.com>
2023-02-19 19:09:25 +0800

    third commit

--- src/main/java/com/atguigu/Test.java
+++ src/main/java/com/atguigu/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("你能找到你想要的工作的");
         System.out.println("你会拿到高薪的");
+        System.out.println("你在不久就会拿到offer的");
     }
 }
