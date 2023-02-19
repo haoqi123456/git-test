368667be729e38af25e1d979f617c62b1ab09e6d
ljx <1845617678@qq.com>
2023-02-19 19:22:22 +0800

    hot-fix first commit

--- src/main/java/com/atguigu/Test.java
+++ src/main/java/com/atguigu/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("你能找到你想要的工作的");
         System.out.println("你会拿到高薪的");
         System.out.println("你在不久就会拿到offer的");
+        System.out.println("hot-fix 你会找到和你相爱的人");
     }
 }
